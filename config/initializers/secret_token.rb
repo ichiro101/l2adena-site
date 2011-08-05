5be687cfdcc31d5fefb0f35e9cecd2bdd2d2cd6a
Ichiro Yamamoto <ichiyamamogo@gmail.com>
2011-08-04 23:00:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+L2adena::Application.config.secret_token = 'af6f21d1944f11df40cc7ca98ac4fbdbcbc27be44495f7bf98824974c0f1e8fbb673e165d1fb35077580cb44fde618cd6e6e39cfbed4a7393f1b1078e5a1e9fa'

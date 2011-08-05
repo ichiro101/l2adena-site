e7f5c50a08f49ee63ec8143a9647333cd90f2281
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-05 15:03:34 -0600

    list of all roles, more tests

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe UserRole do
 
+  it "has user_id attribute" do
+    user_roles = UserRole.new
+    user_roles.user_id = 5
+    user_roles.user_id.should == 5
+  end
 end

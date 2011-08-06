eec399f882a4a7af4a7d3d24320f5075609d28d8
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-05 22:59:43 -0600

    permission system complete

--- app/models/role.rb
+++ app/models/role.rb
@@ -27,6 +27,18 @@ class Role
     role_list
   end
 
+  def self.find(id)
+    roles = Role.all
+
+    roles.each do |role|
+      if role.id == id
+        return role
+      end
+    end
+
+    nil
+  end
+
   def self.find_by_symbol(symbol)
     roles = Role.all
 
@@ -39,6 +51,40 @@ class Role
     nil
   end
 
+  def assign_permission(symbol)
+    permission = Permission.find_by_symbol(symbol)
+
+    if permission == nil
+      return nil
+    end
+
+    permission_roles = PermissionRole.where(:permission_id => permission.id, :role_id => self.id)
+
+    if permission_roles.size == 0
+      permission_role = PermissionRole.new
+      permission_role.permission_id = permission.id
+      permission_role.role_id = self.id
+      return permission_role.save
+    end
+
+    true
+  end
+
+  def has_permission?(symbol)
+    permission = Permission.find_by_symbol(symbol)
+
+    if permission == nil
+      raise StandardError, "Permission does not exist"
+    end
+
+    permission_roles = PermissionRole.where(:permission_id => permission.id, :role_id => self.id)
+
+    if permission_roles.size > 0
+      return true
+    else
+      return false
+    end
+  end
 
   attr_accessor :id
   attr_accessor :symbol
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,52 @@ class User < ActiveRecord::Base
   # Callbacks
   before_save :encrypt_password
 
+  # Assign the role of :symbol to the user
+  def assign_role(symbol)
+    role = Role.find_by_symbol(symbol)
+
+    if role == nil
+      return nil
+    end
+
+    user_roles = UserRole.where(:user_id => self.id, :role_id => role.id)
+    if user_roles.size > 0
+      return true
+    end
+
+    user_role = UserRole.new
+    user_role.user_id = self.id
+    user_role.role_id = role.id
+    user_role.save
+  end
+
+  # Check if the user has the role of :symbol
+  def has_role?(symbol)
+    role = Role.find_by_symbol(symbol)
+    user_roles = UserRole.where(:user_id => self.id, :role_id => role.id)
+
+    if user_roles.size > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def can_access?(symbol)
+    user_roles = UserRole.where(:user_id => self.id)
+
+    user_roles.each do |user_role|
+      role = Role.find(user_role.role_id)
+      if role.has_all_permissions == true
+        return true
+      elsif role.has_permission?(symbol)
+        return true
+      end
+    end
+
+    false
+  end
+
   def self.authenticate(username, password)
     users = User.where(:username => username)
     if users.size == 0

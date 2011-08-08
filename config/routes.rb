102d34e1a68191906bd40d95ab58340b3406dccf
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-08 00:39:34 -0600

    delete all links to old admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ L2adena::Application.routes.draw do
   match '/auth/:service/callback' => 'services#create'
   match '/auth/failure' => 'services#failure'
 
-  match '/admin/user/:action(/:id)' => 'admin/user'
-  match '/admin/:action(/:id)' => 'admin/home'
-
   resources :services, :only => [:index, :create, :destroy] do
     collection do
       get 'signin'

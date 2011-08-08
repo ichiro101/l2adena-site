ae0d8952c5a0198c0ec36c8d6619287a27c148ab
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-07 22:34:43 -0600

    adding custom configs for login server configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
+# We need to load the exception class first
+require File.expand_path('../../lib/exceptions', __FILE__)
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 L2adena::Application.initialize!
+
+# Load custom config file
+require File.expand_path('../l2adena_config', __FILE__)

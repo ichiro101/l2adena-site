f26cce3abeead5cc5b3324f5c0e17dd3840dfa54
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-11 19:19:36 -0600

    news working, now working on the html editing of news

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,7 +1,7 @@
 class Admin::NewsController < Admin::AdminController
 
   def index
-    @news_items = News.paginate(:page => params[:page])
+    @news_items = News.paginate(:page => params[:page]).order("created_at desc")
   end
 
   def show
@@ -10,6 +10,39 @@ class Admin::NewsController < Admin::AdminController
 
   def new
     @news = News.new
+  end
+
+  def create
+    @news = News.new
+
+    @news.name = params[:news][:name]
+    @news.user_id = current_user.id
+    @news.content = params[:news][:content]
+
+    if @news.save
+      flash[:success] = "News has been posted"
+      redirect_to admin_news_index_path
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @news = News.find(params[:id])
+  end
+
+  def update
+    @news = News.find(params[:id])
+
+    @news.name = params[:news][:name]
+    @news.content = params[:news][:content]
+
+    if @news.save
+      flash[:success] = "News item has been updated"
+      redirect_to admin_news_index_path
+    else
+      render('edit')
+    end
   end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @title = "L2Adena Home Page"
+    @news_items = News.paginate(:page => params[:page]).order("created_at DESC")
   end
 
   def show_page

218b38464bb24ef40915cc345ee636f4ef8f0307
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-20 00:34:53 -0600

    ticket system view

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,6 +2,11 @@ class TicketsController < ApplicationController
 
   def index
     @tickets = Ticket.all
+    @ticket = Ticket.new
+  end
+
+  def create
+
   end
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,18 @@ class Ticket < ActiveRecord::Base
   class TicketStatus
     attr_accessor :id
     attr_accessor :symbol
+    attr_accessor :label
+
+    def all
+      all_ticket_status = Array.new
+
+      resolved = Array.new
+      resolved.id = 1
+      resolved.symbol = :resolved
+      resolved.label = "Resolved"
+      all_ticket_status << resolved
+
+    end
   end
 
 end

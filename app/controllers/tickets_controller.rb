c17d59d86fe665fd758ea28f0a87eb91c29d68c3
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-20 12:36:48 -0600

    ticket model

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,12 +1,14 @@
 class TicketsController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def index
     @tickets = Ticket.all
     @ticket = Ticket.new
   end
 
   def create
-
+    @ticket = Ticket.new
   end
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,17 +4,54 @@ class Ticket < ActiveRecord::Base
     attr_accessor :id
     attr_accessor :symbol
     attr_accessor :label
+    attr_accessor :active
 
-    def all
+    def self.all
       all_ticket_status = Array.new
 
-      resolved = Array.new
+      resolved = TicketStatus.new
       resolved.id = 1
       resolved.symbol = :resolved
       resolved.label = "Resolved"
+      resolved.active = false
       all_ticket_status << resolved
 
+      invalid = TicketStatus.new
+      invalid.id = 2
+      invalid.symbol = :invalid
+      invalid.active = false
+      invalid.label = "Invalid"
+      all_ticket_status << invalid
+
+      more_info = TicketStatus.new
+      more_info.id = 3
+      more_info.symbol = :more_info
+      more_info.active = true
+      more_info.label = "More information is needed"
+      all_ticket_status << more_info
+
+      active = TicketStatus.new
+      active.id = 4
+      active.symbol = :active
+      active.active = true
+      active.label = "Active"
+      all_ticket_status << active
+
+      all_ticket_status
+    end
+
+    def self.find_by_symbol(symbol)
+      all_items = TicketStatus.all
+      all_items.each do |item|
+        return item if item.symbol == symbol
+      end
+      nil
     end
   end
+
+  validates_presence_of :name
+  validates_presence_of :content
+  validates_presence_of :status
+  validates_presence_of :user_id
 
 end

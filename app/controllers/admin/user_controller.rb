a6abc3798ba763caac503b287ac48b9e82e00817
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2011-08-21 16:27:38 -0600

    web account deletion

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -8,4 +8,11 @@ class Admin::UserController < Admin::AdminController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:notice] = "User has been deleted from the database, please note the associated game accounts were NOT deleted"
+    redirect_to admin_user_index_path
+  end
+
 end
